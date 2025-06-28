a4978fd5222689be3d11b1d30f6971f4e0a3e085
Uyhuynh23 <htuy23@apcs.fitus.edu.vn>
2025-06-28 12:12:59 +0700

    SplashScreen(DONE) + Hompage(draft)

---
+++ app/src/main/java/com/example/mobile_midterm/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.mobile_midterm
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobile_midterm.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000) // Show splash for 3 seconds
+    }
+}
